10f824b1ac40b24402a9c1ac9c849c1676c517c9
gaza gumbs <gaza@gazas-mbp.attlocal.net>
2019-10-06 21:30:45 -0400

    working

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,17 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import logo from "./logo.svg";
-// import images from "./images/junk_food_subject_graphic.png";
-import images from "./images/soccer_stadium_subject_graphic.png";
-// import images3 from "./images/theme_park_subject_graphic.png";
+import images from "./images/junk_food_subject_graphic.png";
+import images2 from "./images/soccer_stadium_subject_graphic.png";
+import images3 from "./images/theme_park_subject_graphic.png";
 
-// import './App.css';
+import "./App.css";
 
 function App() {
   const [lessons, setLessons] = useState({});
-  const [api, setapi] = useState(false);
+  const [pageLoaded, setPageLoaded] = useState(false);
+  const [filter, setfilter] = useState("");
+
   useEffect(() => {
     axios
       .get("/api")
@@ -17,7 +19,7 @@ function App() {
         // handle success
         console.log(response);
         setLessons(response.data);
-        setapi(true);
+        setPageLoaded(true);
       })
       .catch(function(error) {
         // handle error
@@ -27,26 +29,58 @@ function App() {
         // always executed
       });
   }, []);
+
+  let filterLesson = event => {
+    const { name, value } = event.target;
+    setfilter(value);
+  };
   return (
-    <div className="App">
-      {api
-        ? lessons.map((data, i) => (
-            <div className="station" key={i}>
-              <li>
-                {data.name}
-                <br />
-                {data.standard}
-                <img
-                  src={images}
-                  alt={data.name}
-                  style={{ height: 80, width: 80, margin: 30 }}
-                />
-              </li>
-              <br />
-              <br />
-            </div>
-          ))
-        : ""}
+    <div>
+      <div className="App">
+        {pageLoaded ? (
+          lessons.map((data, i) => {
+            if (data.standard.includes(filter.toUpperCase())) {
+              return (
+                <div key={i}>
+                  <li>
+                    {data.name}
+                    <br />
+                    {data.standard}
+                  </li>
+                  <img
+                    src={require(`./${data.image}`)}
+                    alt={data.name}
+                    style={{ height: 200, width: 200, marginLeft: 50 }}
+                  />
+                </div>
+              );
+            }
+          })
+        ) : (
+          <div>No Matches</div>
+        )}
+      </div>
+      <div>
+        <br />
+        <br />
+        <br />
+        Search Below
+        <br />
+        <input
+          type="text"
+          style={{
+            width: 500,
+            height: 30,
+            marginTop: 40,
+            marginLeft: 40,
+            fontSize: 30
+          }}
+          className="form-control"
+          name="filter"
+          onChange={filterLesson}
+          value={filter}
+        />
+      </div>
     </div>
   );
 }
--- index.js
+++ index.js
@@ -6,13 +6,6 @@ var logger = require("morgan");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-let db = new sqlite3.Database("./lessons.sqlite3", err => {
-  if (err) {
-    return console.error(err.message);
-  }
-  console.log("Connected to the in-memory SQlite database.");
-});
-
 app.use(logger("dev"));
 // Configure body parser for AJAX requests
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -21,6 +14,13 @@ app.use(bodyParser.json());
 app.use(express.static("client/build"));
 // Add routes, both API and view
 app.get("/api", (req, res) => {
+  let db = new sqlite3.Database("./lessons.sqlite3", err => {
+    if (err) {
+      return console.error(err.message);
+    }
+    console.log("Connected to the SQlite database.");
+  });
+
   db.all(
     "select DISTINCT  name, standard, image  FROM api_lesson  LEFT JOIN api_screen ON api_lesson.id = api_screen.lesson_id",
     [],
@@ -28,6 +28,12 @@ app.get("/api", (req, res) => {
       res.send(rows);
     }
   );
+  db.close(err => {
+    if (err) {
+      return console.error(err.message);
+    }
+    console.log("Close the database connection.");
+  });
 });
 
 // Start the API server
@@ -41,9 +47,3 @@ app.listen(PORT, function() {
 //   console.log(rows);
 // });
 // // close the database connection
-// db.close(err => {
-//   if (err) {
-//     return console.error(err.message);
-//   }
-//   console.log("Close the database connection.");
-// });
